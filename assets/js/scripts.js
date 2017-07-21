c24bba86cd9cee517312ac9570e6d06b50785083
Victor Tsang <victorwltsang@gmail.com>
2017-07-21 11:16:29 -0700

    js

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $("nav").sticky({topSpacing:0});
+  $('body').scrollspy({ target: 'nav', offset:80});
+
+  $('.scrollto').on('click', function(e){
+
+     //store hash
+     var target = this.hash;
+
+     e.preventDefault();
+
+ $('body').scrollTo(target, 800, {offset: -60, 'axis':'y'});
+
+});
+});
