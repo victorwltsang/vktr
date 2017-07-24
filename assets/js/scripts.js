65e838bec81702da7016e9793e328235575a144d
Victor Tsang <victorwltsang@gmail.com>
2017-07-23 22:25:41 -0700

    added some model

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,29 @@
+$(document).ready(function() {
+  $("nav").sticky({
+    topSpacing: 0
+  });
+  $('body').scrollspy({
+    target: 'nav',
+    offset: 80
+  });
 
-$(document).ready(function(){
-  $("nav").sticky({topSpacing:0});
-  $('body').scrollspy({ target: 'nav', offset:80});
+  $('.scrollto').on('click', function(e) {
 
-  $('.scrollto').on('click', function(e){
+    //store hash
+    var target = this.hash;
 
-     //store hash
-     var target = this.hash;
+    e.preventDefault();
 
-     e.preventDefault();
+    $('body').scrollTo(target, 800, {
+      offset: -60,
+      'axis': 'y'
+    });
 
- $('body').scrollTo(target, 800, {offset: -60, 'axis':'y'});
+  });
 
-});
+//   $(".cards").click(function() {
+//   window.open($(this).find("a").attr("href"));
+//   return false;
+// });
+
 });
